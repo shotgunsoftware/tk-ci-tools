7e7830dab49a921b7a4bb3646f23071a6c7f3a9e
Jean-François Boismenu <jean.francois.boismenu@autodesk.com>
2020-09-25 13:25:16 -0400

    now making a request to actually validate credentials

--- tests/test_credentials.py
+++ tests/test_credentials.py
@@ -13,4 +13,5 @@ from tk_toolchain.authentication import get_toolkit_user
 # This test is meant to be called from the command-line and not by pytest.
 if __name__ == "__main__":
     # This ensures that tk-toolchain was able to build credentials that are valid.
-    get_toolkit_user().create_sg_connection()
+    # Hit the server just to make sure the credentials are all good.
+    get_toolkit_user().create_sg_connection().find_one("HumanUser", [])
