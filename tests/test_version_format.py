32c9b6c675b225edd99a283db7606ec39d669363
Julien Langlois <julien.langlois@autodesk.com>
2026-01-29 13:31:04 -0800

    fixup! fixup! fixup! Change strategy - store the Python script inline

--- tests/test_version_format.py
+++ tests/test_version_format.py
@@ -78,9 +78,7 @@ def validate_version_format() -> typing.Generator[types.ModuleType, None, None]:
             # Import the module dynamically
             import importlib.util
 
-            spec = importlib.util.spec_from_file_location(
-                module_name, temp_file_path
-            )
+            spec = importlib.util.spec_from_file_location(module_name, temp_file_path)
             module = importlib.util.module_from_spec(spec)
             sys.modules[module_name] = module
             spec.loader.exec_module(module)
